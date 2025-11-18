399d059499ed10904ee58f5dc0610cd5f53d3a68
satya-git07 <sbappadala@softility.com>
2025-11-18 19:22:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,20 @@
-resource "aws_instance" "web" {
-  ami           = "ami-08b5b3a93ed654d19"
-  instance_type = "t3.micro"
+#resource "aws_instance" "web" {
+ # ami           = "ami-08b5b3a93ed654d19"
+  #instance_type = "t3.micro"
 
-  tags = {
-    Name = "HelloWorld"
+  #tags = {
+   # Name = "HelloWorld"
+ # }
+#}
+
+
+terraform {
+  backend "http" {
+    address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/SFTY_Training/workspaces/awsstatemigration/terraform-backend?accountIdentifier=ucHySz2jQKKWQweZdXyCog"
+    username = "harness"
+    lock_address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/SFTY_Training/workspaces/awsstatemigration/terraform-backend/lock?accountIdentifier=ucHySz2jQKKWQweZdXyCog"
+    lock_method = "POST"
+    unlock_address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/SFTY_Training/workspaces/awsstatemigration/terraform-backend/lock?accountIdentifier=ucHySz2jQKKWQweZdXyCog"
+    unlock_method = "DELETE"
   }
 }
-
