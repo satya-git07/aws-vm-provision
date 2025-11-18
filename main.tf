8a3f72c28a7ed353ad3f91a4c9cb2da80c445ee2
satya-git07 <sbappadala@softility.com>
2025-11-18 19:48:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,15 @@ resource "aws_instance" "web" {
   }
 }
 
+resource "aws_ebs_volume" "web_data_disk" {
+  # Get the AZ from the EC2 instance resource
+  availability_zone = aws_instance.web.availability_zone 
+  
+  size              = 20 
+  type              = "gp3"
+  encrypted         = true
+
+  tags = {
+    Name = "${aws_instance.web.tags.Name}-Data-Disk"
+  }
+}
